b0ebe9f0227a9c3d913d5d8cd632df145bc23f38
tamim <tamim.shahriar@gmail.com>
2019-08-12 01:24:22 +0800

    max distance python solution

---
+++ max_distance/solution.py
@@ -0,0 +1,28 @@
+def maximumGap(A):
+    numbers = []
+    for i, num in enumerate(A):
+        numbers.append((num, i))
+    numbers.sort()
+    
+    max_gap = 0
+    min_number = numbers[0][1]
+
+    for item in numbers:
+        num = item[1]
+        if num <= min_number:
+            min_number = num
+        else:
+            max_gap = max(max_gap, num - min_number)
+    return max_gap
+
+
+
+if __name__ == "__main__":
+    A = [3, 5, 4, 2]
+    print(maximumGap(A)) # 2
+
+    A = [2, 3, 4, 5]
+    print(maximumGap(A)) # 
+
+    A = [5, 4, 3, 2]
+    print(maximumGap(A)) # 
