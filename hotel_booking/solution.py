93f7caee97b14319cabba6008c24cfada6b2c61d
tamim <tamim.shahriar@gmail.com>
2019-08-13 16:58:28 +0800

    added merge intervals and hote bookings (both from interviewbit)

---
+++ hotel_booking/solution.py
@@ -0,0 +1,58 @@
+"""
+Accepted solution
+"""
+def hotel(arrive, depart, K):
+    arrive.sort()
+    depart.sort()
+    
+    count = 0
+    n = len(arrive)
+    
+    i, j = 0, 0
+    while i < n and j < n:
+        if arrive[i] < depart[j]:
+            count += 1
+            if count > K:
+                return False
+            i += 1
+        else:
+            count -= 1
+            j += 1
+
+    return True
+
+
+"""
+Accepted and nicer looking solution (code)
+"""
+def hotel(arrive, depart, K):
+    events = [(t, 1) for t in arrive] + [(t, 0) for t in depart]
+    events.sort()
+
+    count = 0
+
+    for event in events:
+        if event[1] == 1:
+            count += 1
+        else:
+            count -= 1
+
+        if count > K:
+            return 0
+
+    return 1
+
+
+"""
+Inefficent solution (why? ask yourself.)
+"""
+def hotel(arrive, depart, K):
+    days = defaultdict(int)
+    
+    for start, end in zip(arrive, depart):
+        for i in range(start, end):
+            days[i] += 1
+            if days[i] > K:
+                return False
+
+    return True
