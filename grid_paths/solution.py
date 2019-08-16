3b83aea9a6bc4540cdc4068245df8bff86377b27
tamim <tamim.shahriar@gmail.com>
2019-08-16 13:54:55 +0800

    python soluiton for unique grid paths

---
+++ grid_paths/solution.py
@@ -0,0 +1,18 @@
+def uniquePaths(A, B):
+    A -= 1
+    B -= 1
+    
+    def factorial(n):
+        fact = 1
+        
+        for i in range(2, n+1):
+            fact *= i
+            
+        return fact
+        
+    return factorial(A+B) // (factorial(A) * factorial(B))
+
+
+if __name__ == "__main__":
+    A, B = 3, 3
+    print(uniquePaths(3, 3))
