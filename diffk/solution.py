5765d91a83d7e8d96a45fbbbaed355647f37d832
Tamim Shahriar <tamim.shahriar@indebted.co>
2019-12-11 21:24:52 +1100

    diffk

--- diffk/solution.py
+++ diffk/solution.py
@@ -1,3 +1,4 @@
+# Brute force solution, A is sorted
 def diff_possible1(A, k):
     n = len(A)
     i, j = 0, 1
@@ -15,6 +16,7 @@ def diff_possible1(A, k):
     return False
 
 
+# O(n) solution, A is sorted
 def diff_possible2(A, k):
     n = len(A)
     i, j = 0, 1
@@ -27,4 +29,26 @@ def diff_possible2(A, k):
             j += 1
                 
     return False
-                
+
+# O(n) solution, A is not sorted, space complexity also O(n)
+from collections import defaultdict
+
+def diff_possible3(A, k):
+    if k == 0:
+        if len(A) < 2:
+            return 0
+        
+    dd = defaultdict(int)
+    
+    for n in A:
+        dd[n] += 1
+        
+    for n in A:
+        dd[n] -= 1
+        x = k + n
+        if dd[x]:
+            return 1
+        dd[n] += 1
+            
+    return 0
+            
